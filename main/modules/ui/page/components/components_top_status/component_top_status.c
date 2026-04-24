a6fb53a88918a91250b38b8b8c9cbc14b52f3af6
FredyXu <fredyxu@fredyxu.com>
2026-04-24 10:39:30 +0800

    2026-04-24 10:39:30

---
+++ main/modules/ui/page/components/components_top_status/component_top_status.c
@@ -0,0 +1,13 @@
+#include "component_top_status.h"
+
+#include "lvgl.h"
+#include "esp_err.h"
+#include "config/config_sys.h"
+
+static lv_obj_t *status_body;
+
+esp_err_t ui_add_top_status(lv_obj_t *p) {
+	status_body = lv_obj_crete(p);
+
+	return ESP_OK;
+}
---
+++ main/modules/ui/page/components/components_top_status/component_top_status.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "lvgl.h"
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t ui_add_top_status(lv_obj_t *p);
+
+#ifdef __cplusplus
+}
+#endif
